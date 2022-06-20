951749761fd6cba75fda21a71903e30100f18623
tolulope <tolulope24049@gmail.com>
2022-06-20 10:45:38 -0700

    add

--- opcode_instruction2.c
+++ opcode_instruction2.c
@@ -81,22 +81,22 @@ void _nop(stack_t **doubly, unsigned int cline)
  */
 void _sub(stack_t **doubly, unsigned int cline)
 {
-    int m = 0;
-    stack_t *aux = NULL;
+ int m = 0;
+ stack_t *aux = NULL;
 
-    aux = *doubly;
+ aux = *doubly;
 
-    for (; aux != NULL; aux = aux->next, m++)
+ for (; aux != NULL; aux = aux->next, m++)
         ;
 
-    if (m < 2)
-    {
-        dprintf(2, "L%u: can't sub, stack too short\n", cline);
-        free_vglo();
-        exit(EXIT_FAILURE);
-    }
+ if (m < 2)
+ {
+ dprintf(2, "L%u: can't sub, stack too short\n", cline);
+free_vglo();
+exit(EXIT_FAILURE);
+}
 
-    aux = (*doubly)->next;
-    aux->n -= (*doubly)->n;
-    _pop(doubly, cline);
+aux = (*doubly)->next;
+aux->n -= (*doubly)->n;
+_pop(doubly, cline);
 }
